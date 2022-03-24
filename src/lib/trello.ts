be8351941ce6a4e59f7f9716c0583a0cfff6b0e4
Pablo J Torrubiano <me@pablotorrubiano.xyz>
2022-03-24 01:20:02 +0100

    Configured request rate limiter and added card and list creation methods

--- src/lib/trello.ts
+++ src/lib/trello.ts
@@ -1,14 +1,17 @@
+import { RateLimiter } from 'limiter';
+
 import { ApiFn, QueryParams } from '../types/api';
-import { Board } from '../types/trello';
+import { Board, Card, List } from '../types/trello';
 import { get, post } from '../utils/api';
 import logger from '../utils/logger';
 
 const { TRELLO_API_KEY: apiKey, TRELLO_API_TOKEN: apiToken } = process.env;
 
+const trelloLimiter = new RateLimiter({ tokensPerInterval: 1, interval: 150 });
 const trelloApiBaseUrl = 'https://api.trello.com';
 const trelloAuthToken = `OAuth oauth_consumer_key="${apiKey}", oauth_token="${apiToken}"`;
 
-const trelloApiCall = (
+const trelloApiCall = async (
   methodFn: ApiFn,
   route: string,
   queryParams?: QueryParams,
@@ -18,6 +21,7 @@ const trelloApiCall = (
       'Trello API Key or API Token are undefined. Check that TRELLO_API_KEY and TRELLO_API_TOKEN exist in .env file or are passed by param',
     );
   }
+  await trelloLimiter.removeTokens(1);
   return methodFn(
     `${trelloApiBaseUrl}${route}`,
     trelloAuthToken,
@@ -26,9 +30,10 @@ const trelloApiCall = (
 };
 
 export const checkConnection = async () => {
+  const route = '/1/members/me';
   try {
     logger.info('Connecting to Trello API');
-    await trelloApiCall(get, '/1/members/me');
+    await trelloApiCall(get, route);
     logger.info('Successfully connected to Trello API');
   } catch (error) {
     logger.error(`Couldn't connect to Trello API. Error: ${error}`);
@@ -36,13 +41,54 @@ export const checkConnection = async () => {
 };
 
 export const createBoard = async (name: string) => {
+  const route = '/1/boards/';
   try {
     logger.info(`Creating board with name: ${name}`);
-    const board = await trelloApiCall(post, '/1/boards/', { name }) as Board;
+    const board = await trelloApiCall(post, route, { name }) as Board;
     logger.info(`Successfully created board ${name}`);
     return board;
   } catch (error) {
-    logger.error(`Couldn't create Board. Error: ${error}`);
+    logger.error(`Couldn't create board ${name}. Error: ${error}`);
+    return null;
+  }
+};
+
+export const createList = async (name: string, idBoard: string) => {
+  const route = '/1/lists';
+  try {
+    logger.info(`Creating list with name: ${name} in board: ${idBoard}`);
+    const list = await trelloApiCall(post, route, { name, idBoard });
+    logger.info(`Successfully created list ${name}`);
+    return list as List;
+  } catch (error) {
+    logger.error(`Couldn't create list ${name}. Error: ${error}`);
+    return null;
+  }
+};
+
+export const createCard = async (name: string, idList: string) => {
+  const route = '/1/cards';
+  try {
+    logger.info(`Creating card with name: ${name} in list: ${idList}`);
+    const card = await trelloApiCall(post, route, { name, idList });
+    logger.info(`Successfully created card ${name}`);
+    return card as Card;
+  } catch (error) {
+    logger.error(`Couldn't create card ${name}. Error: ${error}`);
+    return null;
+  }
+};
+
+// TODO: make this work
+export const addCardCover = async (idCard: string, file: string) => {
+  const route = `/1/cards/${idCard}/attachments`;
+  try {
+    logger.info(`Adding card cover to card ID: ${idCard}`);
+    const list = await trelloApiCall(post, route, { file, setCover: true }) as Board;
+    logger.info(`Successfully added cover to card ID: ${idCard}`);
+    return list;
+  } catch (error) {
+    logger.error(`Couldn't add cover to card  ID: ${idCard}. Error: ${error}`);
     return null;
   }
 };

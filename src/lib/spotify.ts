1e13d4ee02ed3ed55db596f397b2b199a0bdb278
Pablo J Torrubiano <me@pablotorrubiano.xyz>
2022-03-24 12:48:48 +0100

    Added spotify lib and types

---
+++ src/lib/spotify.ts
@@ -0,0 +1,82 @@
+import { RateLimiter } from 'limiter';
+
+import { ApiFn, DataParams } from '../types/api';
+import { AlbumSearch, AuthResponse } from '../types/spotify';
+import { get, post } from '../utils/api';
+import logger from '../utils/logger';
+
+const {
+  SPOTIFY_CLIENT_ID: clientId,
+  SPOTIFY_CLIENT_SECRET: clientSecret,
+  SPOTIFY_MAX_REQUEST_RATE: maxRate,
+} = process.env;
+
+const spotifyLimiter = new RateLimiter({
+  tokensPerInterval: 1,
+  interval: 1000 / (parseInt(maxRate || '8', 10)),
+});
+
+// The auth token has to be asked to Spotify. Will be set when connectToSpotify fn is exec
+let spotifyAuthToken: string;
+
+const spotifyAuthTokenUrl = 'https://accounts.spotify.com/api/token';
+
+export const connectToSpotify = async () => {
+  if (!clientId || !clientSecret) {
+    throw Error(
+      'Spotify Client ID or Client Secret is undefined. Check that SPOTIFY_CLIENT_ID and SPOTIFY_CLIENT_SECRET exist in .env file or are passed by param',
+    );
+  }
+  try {
+    logger.info('Connecting to Spotify');
+    // Retrieve auth token from spotify
+    const authResponse = await post(
+      spotifyAuthTokenUrl,
+      `Basic ${Buffer.from(`${clientId}:${clientSecret}`).toString('base64')}`,
+      undefined,
+      { grant_type: 'client_credentials' },
+      'application/x-www-form-urlencoded',
+    ) as AuthResponse;
+    const { access_token: accessToken, token_type: tokenType } = authResponse;
+
+    spotifyAuthToken = `${tokenType} ${accessToken}`;
+    logger.info('Successfully connected to Spotify');
+  } catch (error) {
+    logger.error(`Couldn't connect to Spotify. Error: ${error}`);
+    throw new Error('Spotify connection Error');
+  }
+};
+
+const spotifyApiBaseUrl = 'https://api.spotify.com';
+
+const spotifyApiCall = async (
+  methodFn: ApiFn,
+  route: string,
+  queryParams?: DataParams,
+) => {
+  if (!spotifyAuthToken) {
+    logger.error(
+      'Spotify AuthToken is undefined. Trying to connect to Spotify',
+    );
+    await connectToSpotify();
+  }
+  await spotifyLimiter.removeTokens(1);
+  return methodFn(
+    `${spotifyApiBaseUrl}${route}`,
+    spotifyAuthToken,
+    queryParams,
+  );
+};
+
+export const searchAlbum = async (query: string) => {
+  const route = '/v1/search';
+  try {
+    logger.info(`Searching album with title: ${query}`);
+    const queryResult = await spotifyApiCall(get, route, { q: `album:${query}`, type: 'album' }) as AlbumSearch;
+    logger.info(`Search successful. Found ${queryResult.albums.total} albums with title ${query}`);
+    return queryResult;
+  } catch (error) {
+    logger.error(`Search failed. Error: ${error}`);
+    return null;
+  }
+};
---
+++ src/types/spotify.d.ts
@@ -0,0 +1,48 @@
+export interface AuthResponse {
+  access_token: string;
+  token_type: string;
+  expires_in: number;
+}
+
+export interface Artist {
+  external_urls: unknown;
+  href: string;
+  id: string;
+  name: string;
+  type: string;
+  uri: string;
+}
+
+export interface AlbumImage {
+  height: number;
+  url: string;
+  width: number;
+}
+
+export interface Album {
+  album_type: string;
+  artists: Artist[];
+  available_markets: string[];
+  external_urls: Record<string, string>;
+  href: string;
+  id: string;
+  images: AlbumImage[],
+  name: string;
+  release_date: string;
+  release_date_precision: string;
+  total_tracks: number,
+  type: string;
+  uri: string;
+}
+
+export interface AlbumSearch {
+  albums: {
+    href: string;
+    items: Album[];
+    limit: number;
+    next: null;
+    offset: number;
+    previous: null;
+    total: number;
+  }
+}

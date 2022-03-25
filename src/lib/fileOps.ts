4eeca0b1df6e5d1d547a68031a4fd16e8650ad91
Pablo J Torrubiano <me@pablotorrubiano.xyz>
2022-03-25 14:38:36 +0100

    Renamed file

--- src/lib/fileOps.ts
+++ src/lib/fileOps.ts
@@ -1,20 +1,32 @@
 import { readFileSync } from 'fs';
+import { Album } from '../types/album';
 
 import logger from '../utils/logger';
 
+// from https://stackoverflow.com/questions/43118692/typescript-filter-out-nulls-from-an-array
+const notEmpty = <TValue>(
+  value: TValue | null | undefined,
+): value is TValue => value !== null && value !== undefined;
+
+/**
+ * Given the path of a text file with structure <year> <album-title> it generates an array of
+ * Album objects
+ * @param path filepath of the input file
+ * @returns an array of Albums with year and title
+ */
 const getAlbumsFromFile = (path: string) => {
   // RegEx reminder:
-  //  /s* 0 to n whitespaces
+  //  /s*   0 to n whitespaces
   //  (/d+) 1 to n digits
-  //  /s+ 1 to n whitespaces
-  //  (.+) 1 to n characters
-  //  /s* 0 to n whitespaces
+  //  /s+   1 to n whitespaces
+  //  (.+)  1 to n characters
+  //  /s*   0 to n whitespaces
   const format = /^\s*(\d+)\s+(.+)\s*$/;
   let error = false;
 
   logger.info(`Reading file ${path}`);
   const fileLines = readFileSync(path, 'utf-8').split(/\r?\n/); // split file by line
-  const albumArray = fileLines.map((line, index) => {
+  const albumArray: (Album | null)[] = fileLines.map((line, index) => {
     // empty line
     if (line === '') return null;
     const album = format.exec(line);
@@ -30,7 +42,7 @@ const getAlbumsFromFile = (path: string) => {
       title: album[2],
     };
   });
-  const filteredAlbumArray = albumArray.filter((album) => album !== null);
+  const filteredAlbumArray = albumArray.filter(notEmpty);
   logger.info(`Read finished ${error ? 'with errors' : 'succesfully'}.`);
   logger.info(`${filteredAlbumArray.length} albums were found.`);
   return filteredAlbumArray;

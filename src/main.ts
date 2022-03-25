9fc223596b67df8704a28c2e29e586c90d3f6de9
Pablo J Torrubiano <me@pablotorrubiano.xyz>
2022-03-25 14:46:18 +0100

    Updated main.ts with working execution sequence

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,27 @@ import 'dotenv/config';
 import { exit } from 'process';
 
 import logger from './utils/logger';
-import { checkConnection, createBoard } from './lib/trello';
+import getAlbumsFromFile from './lib/fileOps';
+import { connectToSpotify } from './lib/spotify';
+import { checkTrelloConnection } from './lib/trello';
+import {
+  getCoversFromSpotify,
+  postAlbumsToTrello,
+  updateTrelloCardsWithAlbumCovers,
+} from './lib/albumOps';
 
 const main = async () => {
+  const filePath = `${__dirname}/../${process.env.FILENAME}`;
   logger.info('Initializing app');
-  await checkConnection();
-  await createBoard('Test board');
-  logger.info('Goodbye!');
+  logger.info('Checking connections');
+  await connectToSpotify();
+  await checkTrelloConnection();
+  logger.info('Connections OK! Starting script.');
+  const albumArray = getAlbumsFromFile(filePath);
+  const coverStore = getCoversFromSpotify(albumArray);
+  const trelloBoard = await postAlbumsToTrello(albumArray);
+  await updateTrelloCardsWithAlbumCovers(trelloBoard.cards, await coverStore);
+  logger.info('Script finished. Goodbye!');
   exit(0);
 };
 
